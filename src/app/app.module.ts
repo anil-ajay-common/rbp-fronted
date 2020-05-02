ec35b8e36c019283469fa59d1c76da8f82bf3402
ascsoftdev <ascsoftdev@gmail.com>
2020-05-02 15:24:00 +0530

    501: Added some compnent and prepare dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { StateViewComponent } from './components/state-view/state-view.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, HeaderComponent, StateViewComponent, RoutingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
     SharedModule,
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+],
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,37 @@ import {MatButtonModule} from '@angular/material/button'
 import { MatCommonModule } from '@angular/material/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import {MatTableModule} from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-    imports: [MatButtonModule,MatCommonModule, MatAutocompleteModule, MatTableModule],
-    exports: [MatButtonModule,MatCommonModule, MatAutocompleteModule, MatTableModule]
+    imports: [
+            FormsModule,
+            ReactiveFormsModule,
+            MatButtonModule,
+             MatCommonModule,
+             MatAutocompleteModule,
+             MatTableModule,
+             MatTableModule,
+             MatTooltipModule,
+             MatCheckboxModule,
+             MatToolbarModule,
+             MatRadioModule],
+    exports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatCommonModule,
+        MatAutocompleteModule,
+        MatTableModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatRadioModule]
 })
 export class SharedModule {}
